8dfa6c99eb2696797b8685a55e561c00ceab9b37
Jim Jin <jim_king_2000@163.com>
2022-07-21 12:14:15 +0800

    add tip

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,5 +1,5 @@
-import { Box, Stack, Button, Video } from 'grommet';
-import { Expand } from 'grommet-icons';
+import { Box, Stack, Button, Video, Tip } from 'grommet';
+import { Expand, Camera, ZoomIn, VolumeMute, Microphone } from 'grommet-icons';
 
 export function VideoPlayer() {
   return (
@@ -7,8 +7,12 @@ export function VideoPlayer() {
       <Box width='640px' height='480px' background={'dark-6'} border>
         <Video controls={false} />
       </Box>
-      <Box margin='small' gap='small'>
-        <Button plain icon={<Expand />} />
+      <Box direction='row' margin='small' gap='small'>
+        <Tip key={0} content='抓图'><Button plain icon={<Camera />} /></Tip>
+        <Tip key={1} content='放大'><Button plain icon={<ZoomIn />} /></Tip>
+        <Tip key={2} content='扬声器静音'><Button plain icon={<VolumeMute />} /></Tip>
+        <Tip key={3} content='麦克风静音'><Button plain icon={<Microphone />} /></Tip>
+        <Tip key={4} content='全屏'><Button plain icon={<Expand />} /></Tip>
       </Box>
     </Stack>
   );
