0a70173033fc5102d7cf4d2dc6e249dae018e7cd
jim-king-2000 <jim_king_2000@163.com>
2022-07-21 04:15:16 +0000

    Deploying to gh-pages from @ daxiao-cloud/daxiaovisionui@8dfa6c99eb2696797b8685a55e561c00ceab9b37 🚀

---
+++ _next/static/dCU2T7tYVrZkubKw1bbuM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/972-89df52ba5de3894e.js","static/chunks/pages/index-286f2cd99d99029b.js"],"/_error":["static/chunks/pages/_error-e68c014077669324.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
