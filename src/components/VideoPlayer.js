bf6b06af20f63c8d9f08955f8617cf444306a186
Jim Jin <jim_king_2000@163.com>
2022-07-21 14:35:10 +0800

    refactory

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -7,7 +7,7 @@ export function VideoPlayer() {
       <Box width='640px' height='480px' background={'dark-6'} border>
         <Video controls={false} />
       </Box>
-      <Box direction='row' margin='small' gap='small'>
+      <Box round={{ corner: 'top', size: 'xsmall' }} direction='row' pad='small' gap='medium' background='#0000001E'>
         <Tip key={0} content='抓图'><Button plain icon={<Camera />} /></Tip>
         <Tip key={1} content='放大'><Button plain icon={<ZoomIn />} /></Tip>
         <Tip key={2} content='扬声器静音'><Button plain icon={<VolumeMute />} /></Tip>
