0d1fb1e13e5f5a1fa0375d7c88928c9ef03f0105
jim-king-2000 <jim_king_2000@163.com>
2022-07-21 04:56:54 +0000

    Deploying to gh-pages from @ daxiao-cloud/daxiaovisionui@ce1738cf346d944d06caf5ce92030a62abd311b6 🚀

---
+++ _next/static/QEDOpXlwOEsr4MM9Agbkz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/809-c542354cbffa91d1.js","static/chunks/pages/index-99ce1e4c678dca5d.js"],"/_error":["static/chunks/pages/_error-e68c014077669324.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/mv_-JJbNs_4JvcKzsR9EW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/809-c542354cbffa91d1.js","static/chunks/pages/index-fa102341d332b950.js"],"/_error":["static/chunks/pages/_error-e68c014077669324.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
