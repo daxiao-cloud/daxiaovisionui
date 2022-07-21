1fb73f7ddba089e9f18ac7e4b22883a62729b539
Jim Jin <jim_king_2000@163.com>
2022-07-21 11:32:18 +0800

    add icons

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,9 +1,15 @@
-import { Box, Video } from 'grommet';
+import { Box, Stack, Button, Video } from 'grommet';
+import { Expand } from 'grommet-icons';
 
 export function VideoPlayer() {
   return (
-    <Box width='640px' height='480px' background={'dark-6'} border>
-      <Video controls={false} />
-    </Box>
+    <Stack anchor='bottom'>
+      <Box width='640px' height='480px' background={'dark-6'} border>
+        <Video controls={false} />
+      </Box>
+      <Box margin='small' gap='small'>
+        <Button plain icon={<Expand />} />
+      </Box>
+    </Stack>
   );
 }
