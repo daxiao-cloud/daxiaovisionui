aab036af7e14e13f4c5c3399d8b738b7f93da542
Jim Jin <jim_king_2000@163.com>
2022-07-22 16:09:03 +0800

    refactor video player

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,19 +1,23 @@
 import { Box, Stack, Button, Video, Tip } from 'grommet';
 import { Expand, Camera, ZoomIn, VolumeMute, Microphone, Close } from 'grommet-icons';
 
+function IconButton({ content, icon}) {
+  return (<Tip key={0} content={content}><Button plain icon={icon} /></Tip>);
+}
+
 export function VideoPlayer() {
   return (
     <Stack anchor='bottom'>
       <Box width='640px' height='480px' background={'dark-6'} border>
         <Video controls={false} />
       </Box>
-      <Box round={{ corner: 'top', size: 'xsmall' }} direction='row' pad='small' gap='medium' background='#0000001E'>
-        <Tip key={0} content='抓图'><Button plain icon={<Camera />} /></Tip>
-        <Tip key={1} content='放大'><Button plain icon={<ZoomIn />} /></Tip>
-        <Tip key={2} content='扬声器静音'><Button plain icon={<VolumeMute />} /></Tip>
-        <Tip key={3} content='麦克风静音'><Button plain icon={<Microphone />} /></Tip>
-        <Tip key={4} content='全屏'><Button plain icon={<Expand />} /></Tip>
-        <Tip key={5} content='关闭'><Button plain icon={<Close />} /></Tip>
+      <Box round={{ corner: 'top', size: 'xsmall' }} direction='row' pad='small' gap='medium' background='#000000A1'>
+        <IconButton key={0} content='抓图' icon={<Camera color='brand' />} />
+        <IconButton key={1} content='放大' icon={<ZoomIn color='brand' />} />
+        <IconButton key={2} content='扬声器静音' icon={<VolumeMute color='brand' />} />
+        <IconButton key={3} content='开始通话' icon={<Microphone color='brand' />} />
+        <IconButton key={4} content='全屏' icon={<Expand color='brand' />} />
+        <IconButton key={5} content='关闭' icon={<Close color='brand' />} />
       </Box>
     </Stack>
   );
