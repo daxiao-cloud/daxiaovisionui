479a42ba315089563dad972f36e3f9b830c372f3
firewild <firewild@daxiao.com>
2022-09-16 11:17:01 +0800

    table

--- src/components/VideoStream/CameraProperty.js
+++ src/components/VideoStream/CameraProperty.js
@@ -1,47 +1,82 @@
-import { Tabs, Tab, Box, Form, Heading, FormField, Button, Text, TextInput, Table, TableBody, TableRow, TableCell } from 'grommet';
-import { CameraSettings } from './CameraSettings';
+import {
+  Tabs,
+  Tab,
+  Box,
+  Form,
+  Heading,
+  FormField,
+  Button,
+  Text,
+  TextInput,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "grommet";
+import { CameraSettings } from "./CameraSettings";
 
 function CameraName() {
   return (
     <Form>
-      <FormField label='摄像机名称'>
+      <FormField label="摄像机名称">
         <TextInput />
       </FormField>
-      <Button primary type='submit' label='提交' margin='small' />
+      <Button primary type="submit" label="提交" margin="small" />
     </Form>
   );
 }
 
+const LeftCell = ({ children }) => (
+  <TableCell
+    plain={true}
+    style={{
+      width: "80px",
+    }}
+  >
+    {children}
+  </TableCell>
+);
+
 function CameraInfo() {
   return (
-    <Table>
+    <Table style={{ width: "100%", tableLayout: "fixed" }}>
       <TableBody>
         <TableRow>
-          <TableCell>制造商</TableCell>
+          <LeftCell>制造商</LeftCell>
           <TableCell>大晓智能</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell>型号</TableCell>
+          <LeftCell>型号</LeftCell>
           <TableCell>金乌1号</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell>统一SN</TableCell>
-          <TableCell>daxiaocloud.af9d40a5-7a36-5c07-b23a-851cd99fbfa5.1658472806.V1StGXR8_Z5jdHi6B-myT</TableCell>
+          <LeftCell>统一SN</LeftCell>
+          <TableCell
+            style={{
+              display: "block",
+              width: "100%",
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+              textOverflow: "ellipsis",
+            }}
+          >
+            daxiaocloud.af9d40a5-7a36-5c07-b23a-851cd99fbfa5.1658472806.V1StGXR8_Z5jdHi6B-myT
+          </TableCell>
         </TableRow>
         <TableRow>
-          <TableCell>设备SN</TableCell>
+          <LeftCell>设备SN</LeftCell>
           <TableCell>SDP37481934</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell>IPv4地址</TableCell>
+          <LeftCell>IPv4地址</LeftCell>
           <TableCell>192.168.100.127</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell>IPv6地址</TableCell>
+          <LeftCell>IPv6地址</LeftCell>
           <TableCell>fe80::64a6:2309:8880:7903</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell>MAC地址</TableCell>
+          <LeftCell>MAC地址</LeftCell>
           <TableCell>6c:f1:7e:9f:83:a2</TableCell>
         </TableRow>
       </TableBody>
@@ -52,9 +87,15 @@ function CameraInfo() {
 export function CameraProperty({ name }) {
   return (
     <Tabs>
-      <Tab title='信息'><CameraInfo /></Tab>
-      <Tab title='名称'><CameraName /></Tab>
-      <Tab title='设置'><CameraSettings name={name} /></Tab>
+      <Tab title="信息">
+        <CameraInfo />
+      </Tab>
+      <Tab title="名称">
+        <CameraName />
+      </Tab>
+      <Tab title="设置">
+        <CameraSettings name={name} />
+      </Tab>
     </Tabs>
   );
 }
