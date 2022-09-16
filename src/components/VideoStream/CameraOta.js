f65424e871a644ec06f568561b0bab1fff4c0b73
Jim Jin <jim_king_2000@163.com>
2022-09-16 14:39:18 +0800

    refactory

--- src/components/VideoStream/CameraOta.js
+++ src/components/VideoStream/CameraOta.js
@@ -4,7 +4,7 @@ export function CameraOta() {
   return (
     <Box gap='small' align='start'>
       <Text>检测到新固件。</Text>
-      <Button primary fill={false} label='开始升级' />
+      <Button primary label='开始升级' />
     </Box>
   );
 }
