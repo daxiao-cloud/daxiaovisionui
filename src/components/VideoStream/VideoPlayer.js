aa3c0a1dd4e8f8d6b24d0ff6a768ce348f5e841d
Jim Jin <jim_king_2000@163.com>
2022-09-20 13:31:49 +0800

    refactory

--- src/components/VideoStream/VideoPlayer.js
+++ src/components/VideoStream/VideoPlayer.js
@@ -8,18 +8,18 @@ function IconButton({ content, icon }) {
 
 export function VideoPlayer({ ...prop }) {
   return (
-    <Stack anchor='bottom' fill {...prop}>
-      <Box fill background='dark-6'>
+    <Box fill background='dark-6' {...prop}>
+      <Stack anchor='bottom' fill>
         <Video controls={false} />
-      </Box>
-      <Box round={{ corner: 'top', size: 'small' }} direction='row' pad='small' gap='medium' background='dark-1'>
-        <IconButton key={0} content='抓图' icon={<Camera />} />
-        <IconButton key={1} content='放大' icon={<ZoomIn />} />
-        <IconButton key={2} content='扬声器静音' icon={<VolumeMute />} />
-        <IconButton key={3} content='开始通话' icon={<Microphone />} />
-        <IconButton key={4} content='全屏' icon={<Expand />} />
-        <IconButton key={5} content='关闭' icon={<Close />} />
-      </Box>
-    </Stack>
+        <Box round={{ corner: 'top', size: 'small' }} direction='row' pad='small' gap='medium' background='dark-1'>
+          <IconButton key={0} content='抓图' icon={<Camera />} />
+          <IconButton key={1} content='放大' icon={<ZoomIn />} />
+          <IconButton key={2} content='扬声器静音' icon={<VolumeMute />} />
+          <IconButton key={3} content='开始通话' icon={<Microphone />} />
+          <IconButton key={4} content='全屏' icon={<Expand />} />
+          <IconButton key={5} content='关闭' icon={<Close />} />
+        </Box>
+      </Stack>
+    </Box>
   );
 }
--- src/components/VideoStream/VideoWindow.js
+++ src/components/VideoStream/VideoWindow.js
@@ -4,13 +4,19 @@ import { VideoPlayer } from './VideoPlayer';
 export function VideoWindow() {
   return (
     <Box flex overflow='auto'>
-      <Grid fill columns='640px' rows='480px' gap='small' align='center' justify='center' style={{ gridAutoFlow: 'row dense' }}>
+      <Grid fill columns='640px' gap='small' align='center' justify='center' style={{ gridAutoFlow: 'row dense', gridAutoRows: '480px' }}>
         <VideoPlayer key={0} />
         <VideoPlayer key={1} />
         <VideoPlayer key={2} />
         <VideoPlayer key={3} />
-        <VideoPlayer key={4} style={{ gridColumn: 'span 2', gridRow: 'span 2' }} />
+        <VideoPlayer key={4} />
         <VideoPlayer key={5} />
+        {/* <VideoPlayer key={0} />
+        <VideoPlayer key={1} />
+        <VideoPlayer key={2} />
+        <VideoPlayer key={3} />
+        <VideoPlayer key={4} style={{ gridColumn: 'span 2', gridRow: 'span 2' }} />
+        <VideoPlayer key={5} /> */}
       </Grid>
     </Box>
   );
