d536b8ad0acd2ea98c789e38d5cf476e2a595270
Jim Jin <jim_king_2000@163.com>
2022-09-28 16:41:33 +0800

    remove tabs from index

--- src/components/VideoStream/index.js
+++ src/components/VideoStream/index.js
@@ -1,5 +1,4 @@
-import { Box, Tabs, Tab, Layer } from 'grommet';
-import { Monitor } from 'grommet-icons';
+import { Box, Layer } from 'grommet';
 import { useState } from 'react';
 import { VideoSidebarWindow } from './VideoSidebarWindow';
 import { VideoWindow } from './VideoWindow';
@@ -8,37 +7,18 @@ import { CameraProperty } from './CameraProperty';
 export function VideoStream() {
   const [value, setValue] = useState(false);
   return (
-    <Tabs flex>
-      <Tab title='视图1' icon={<Monitor />}>
-        <Box direction='row' fill>
-          <VideoSidebarWindow onClickCameraSetting={(name) => setValue(name)} />
-          <VideoWindow number={12} />
-          {value && <Layer
-            position='right'
-            full='vertical'
-            onClickOutside={() => setValue(false)}
-            onEsc={() => setValue(false)}>
-              <Box pad='small' width={{ min: 'medium' }}>
-                <CameraProperty name={value} />
-              </Box>
-            </Layer>}
-        </Box>
-      </Tab>
-      <Tab title='视图2' icon={<Monitor />}>
-        <Box direction='row' fill>
-          <VideoSidebarWindow onClickCameraSetting={(name) => setValue(name)} />
-          <VideoWindow number={4} />
-          {value && <Layer
-            position='right'
-            full='vertical'
-            onClickOutside={() => setValue(false)}
-            onEsc={() => setValue(false)}>
-              <Box pad='small' width={{ min: 'medium' }}>
-                <CameraProperty name={value} />
-              </Box>
-            </Layer>}
-        </Box>
-      </Tab>
-    </Tabs>
+    <Box direction='row' fill>
+      <VideoSidebarWindow onClickCameraSetting={(name) => setValue(name)} />
+      <VideoWindow number={12} />
+      {value && <Layer
+        position='right'
+        full='vertical'
+        onClickOutside={() => setValue(false)}
+        onEsc={() => setValue(false)}>
+          <Box pad='small' width={{ min: 'medium' }}>
+            <CameraProperty name={value} />
+          </Box>
+        </Layer>}
+    </Box>
   );
 }
